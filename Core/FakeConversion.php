09cb3e17ff33b2c49535aa8bbfdfa8f03400ab1e
Dominik Klapuch <klapuchdominik@gmail.com>
2017-09-26 20:23:15 +0200

    Row to typed array

---
+++ Core/FakeConversion.php
@@ -0,0 +1,18 @@
+<?php
+declare(strict_types = 1);
+namespace Klapuch\Storage;
+
+final class FakeConversion implements Conversion {
+	private $value;
+
+	public function __construct($value = null) {
+		$this->value = $value;
+	}
+
+	/**
+	 * @return mixed
+	 */
+	public function value() {
+		return $this->value;
+	}
+}
---
+++ Core/PgRowToTypedArray.php
@@ -0,0 +1,49 @@
+<?php
+declare(strict_types = 1);
+namespace Klapuch\Storage;
+
+final class PgRowToTypedArray implements Conversion {
+	private $origin;
+	private $compound;
+	private $database;
+
+	public function __construct(Conversion $origin, string $compound, \PDO $database) {
+		$this->origin = $origin;
+		$this->compound = $compound;
+		$this->database = $database;
+	}
+
+	/**
+	 * @return mixed
+	 */
+	public function value() {
+		$row = $this->origin->value();
+		$columns = array_keys($row);
+		$types = $this->types($this->compound);
+		return array_combine(
+			$columns,
+			array_map(
+				function(string $value, string $column) use ($types) {
+					return (new PgStringToScalar($value, $types[$column]))->value();
+				},
+				$row,
+				$columns
+			)
+		);
+	}
+
+	private function types(string $compound): array {
+		return array_column(
+			(new ParameterizedQuery(
+				$this->database,
+				'SELECT attribute_name, data_type
+				FROM information_schema.attributes
+				WHERE udt_name = ?
+				ORDER BY ordinal_position',
+				[$compound]
+			))->rows(),
+			'data_type',
+			'attribute_name'
+		);
+	}
+}
