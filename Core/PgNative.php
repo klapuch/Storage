92c85a79ed5f80667d796fad600f91ffb7af2bd0
Dominik Klapuch <klapuchdominik@gmail.com>
2018-03-14 22:33:03 +0100

    Native type witohut nullable

--- Core/PgNative.php
+++ Core/PgNative.php
@@ -6,11 +6,11 @@ namespace Klapuch\Storage;
 final class PgNative implements Conversion {
 	private $original;
 
-	public function __construct(?string $original) {
+	public function __construct(string $original) {
 		$this->original = $original;
 	}
 
-	public function value(): ?string {
+	public function value(): string {
 		return $this->original;
 	}
 }
