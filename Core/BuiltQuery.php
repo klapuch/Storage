881ebe6bb5f1c6044353dfad64b234c5f87a62f2
Dominik Klapuch <klapuchdominik@gmail.com>
2018-07-08 23:12:11 +0200

    update by SQL

--- Core/BuiltQuery.php
+++ Core/BuiltQuery.php
@@ -6,15 +6,15 @@ namespace Klapuch\Storage;
 use Klapuch\Sql;
 
 /**
- * Query built from SQL clause with parameters
+ * Query built from SQL statement with parameters
  */
 final class BuiltQuery implements Query {
 	private $database;
-	private $clause;
+	private $statement;
 
-	public function __construct(MetaPDO $database, Sql\Clause $clause) {
+	public function __construct(MetaPDO $database, Sql\Statement $statement) {
 		$this->database = $database;
-		$this->clause = $clause;
+		$this->statement = $statement;
 	}
 
 	/**
@@ -23,32 +23,32 @@ final class BuiltQuery implements Query {
 	public function field() {
 		return (new TypedQuery(
 			$this->database,
-			$this->clause->sql(),
-			$this->clause->parameters()->binds()
+			$this->statement->sql(),
+			$this->statement->parameters()->binds()
 		))->field();
 	}
 
 	public function row(int $style = \PDO::FETCH_ASSOC): array {
 		return (new TypedQuery(
 			$this->database,
-			$this->clause->sql(),
-			$this->clause->parameters()->binds()
+			$this->statement->sql(),
+			$this->statement->parameters()->binds()
 		))->row($style);
 	}
 
 	public function rows(int $style = \PDO::FETCH_ASSOC): array {
 		return (new TypedQuery(
 			$this->database,
-			$this->clause->sql(),
-			$this->clause->parameters()->binds()
+			$this->statement->sql(),
+			$this->statement->parameters()->binds()
 		))->rows($style);
 	}
 
 	public function execute(): \PDOStatement {
 		return (new TypedQuery(
 			$this->database,
-			$this->clause->sql(),
-			$this->clause->parameters()->binds()
+			$this->statement->sql(),
+			$this->statement->parameters()->binds()
 		))->execute();
 	}
 }
